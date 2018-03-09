09bef738a8d16c06e75488fdfd8908900c8d7d87
willwinworld <zhangzhouit@163.com>
2018-03-09 12:33:29 +0800

    Second

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,10 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
-
+h_theta = X*theta - y; % h_theta(x) = theta_0 + theta_1 * x
+J = (1 / (2 * m)) * sum(h_theta .* h_theta); 
+% h_theta是行向量，所以sum就相当于将所有元素加起来
+% .* 是对应元素相乘，例：[1,2,3,4] .* [1,2,3,4] = [1,4,9,16] 
 % =========================================================================
 
 end
