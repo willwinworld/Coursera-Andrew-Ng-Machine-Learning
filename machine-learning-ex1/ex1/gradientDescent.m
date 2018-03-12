058fcf0831af2d12e422af67db7a183abb47db94
willwinworld <zhangzhouit@163.com>
2018-03-12 11:18:20 +0800

    gradientDescent.mm

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,9 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    temp1 = alpha * (1 / m) * sum(X * theta - y);
+    temp2 = alpha * (1 / m) * sum((X * theta -y).*X(:,2));
+    theta(1) = theta(1) - temp1;
+    theta(2) = theta(2) - temp2;
 
 
 
