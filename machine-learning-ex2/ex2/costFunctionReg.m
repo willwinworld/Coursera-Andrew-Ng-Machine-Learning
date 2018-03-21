ec46204fafe938fe7928023fe85fcf1e2005bcfd
willwinworld <zhangzhouit@163.com>
2018-03-21 10:10:15 +0800

    week 3 done

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -16,14 +16,16 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+% solution 1
 h = sigmoid(X * theta);
-shift_theta = theta(2:size(theta));
-theta_reg = [0;shift_theta];
-
-J = (1 / m) * sum((-y .* log(h) - (1 - y) .* log(1 - h))) + (lambda / (2 * m)) * theta_reg' * theta_reg;
-grad = (1 / m) * sum((h - y) .* X) + (lambda / m) * theta_reg;
-
-
+left_part = -y' * log(h);
+right_part = (1 - y') * log(1 - h);
+theta_zero = theta;
+theta_zero(1) = 0;
+lambda_cost_part = (lambda / (2 * m)) * sum(theta_zero .^ 2);
+lambda_gradient_part = (lambda / m) * theta_zero;
+J = (1 / m) * sum(left_part - right_part) + lambda_cost_part;
+grad = (1 / m) * (X' * (h - y)) + lambda_gradient_part;
 
 % =============================================================
 
